c013f5db6d37f557c970861781132a33d8afb0af
Admin <admin@jeff-campos.local>
2021-06-19 19:06:14 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Departament;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Departament dep = new Departament(1, "Books");
+		
+		System.out.println(dep);
+	}
+
+}
