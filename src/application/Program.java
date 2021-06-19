7d08b27f012a0010491fa47d54f0e888149edd8e
Admin <admin@jeff-campos.local>
2021-06-19 19:15:28 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departament;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Departament dep = new Departament(1, "Books");
+		Seller seller = new Seller(21, "BoB", "bob@gmail.com", new Date(), 3000.0, dep);
 		
-		System.out.println(dep);
+		System.out.println(seller);
 	}
 
 }
